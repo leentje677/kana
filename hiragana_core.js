f9f374b651a8be40f30ff04f9598c42c8908669e
leentje677 <88562968+leentje677@users.noreply.github.com>
2026-06-17 21:43:31 +0200

    Add files via upload

---
+++ hiragana_core.js
@@ -0,0 +1,36 @@
+const HIRAGANA_DATA = {
+  "Hiragana Gojūon": {
+    "あ": { romaji: ["a"] }, "い": { romaji: ["i"] }, "う": { romaji: ["u"] }, "え": { romaji: ["e"] }, "お": { romaji: ["o"] },
+    "か": { romaji: ["ka"] }, "き": { romaji: ["ki"] }, "く": { romaji: ["ku"] }, "け": { romaji: ["ke"] }, "こ": { romaji: ["ko"] },
+    "さ": { romaji: ["sa"] }, "し": { romaji: ["shi"] }, "す": { romaji: ["su"] }, "せ": { romaji: ["se"] }, "そ": { romaji: ["so"] },
+    "た": { romaji: ["ta"] }, "ち": { romaji: ["chi"] }, "つ": { romaji: ["tsu"] }, "て": { romaji: ["te"] }, "と": { romaji: ["to"] },
+    "な": { romaji: ["na"] }, "に": { romaji: ["ni"] }, "ぬ": { romaji: ["nu"] }, "ね": { romaji: ["ne"] }, "の": { romaji: ["no"] },
+    "は": { romaji: ["ha"] }, "ひ": { romaji: ["hi"] }, "ふ": { romaji: ["fu"] }, "へ": { romaji: ["he"] }, "ほ": { romaji: ["ho"] },
+    "ま": { romaji: ["ma"] }, "み": { romaji: ["mi"] }, "む": { romaji: ["mu"] }, "め": { romaji: ["me"] }, "も": { romaji: ["mo"] },
+    "や": { romaji: ["ya"] }, "ゆ": { romaji: ["yu"] }, "よ": { romaji: ["yo"] },
+    "ら": { romaji: ["ra"] }, "り": { romaji: ["ri"] }, "る": { romaji: ["ru"] }, "れ": { romaji: ["re"] }, "ろ": { romaji: ["ro"] },
+    "わ": { romaji: ["wa"] }, "を": { romaji: ["wo", "o"] }, "ん": { romaji: ["n"] }
+  },
+
+  "Hiragana Dakuon": {
+    "が": { romaji: ["ga"] }, "ぎ": { romaji: ["gi"] }, "ぐ": { romaji: ["gu"] }, "げ": { romaji: ["ge"] }, "ご": { romaji: ["go"] },
+    "ざ": { romaji: ["za"] }, "じ": { romaji: ["ji"] }, "ず": { romaji: ["zu"] }, "ぜ": { romaji: ["ze"] }, "ぞ": { romaji: ["zo"] },
+    "だ": { romaji: ["da"] }, "ぢ": { romaji: ["ji"] }, "づ": { romaji: ["zu"] }, "で": { romaji: ["de"] }, "ど": { romaji: ["do"] },
+    "ば": { romaji: ["ba"] }, "び": { romaji: ["bi"] }, "ぶ": { romaji: ["bu"] }, "べ": { romaji: ["be"] }, "ぼ": { romaji: ["bo"] },
+    "ぱ": { romaji: ["pa"] }, "ぴ": { romaji: ["pi"] }, "ぷ": { romaji: ["pu"] }, "ぺ": { romaji: ["pe"] }, "ぽ": { romaji: ["po"] }
+  },
+
+  "Hiragana Yōon": {
+    "きゃ": { romaji: ["kya"] }, "きゅ": { romaji: ["kyu"] }, "きょ": { romaji: ["kyo"] },
+    "しゃ": { romaji: ["sha"] }, "しゅ": { romaji: ["shu"] }, "しょ": { romaji: ["sho"] },
+    "ちゃ": { romaji: ["cha"] }, "ちゅ": { romaji: ["chu"] }, "ちょ": { romaji: ["cho"] },
+    "にゃ": { romaji: ["nya"] }, "にゅ": { romaji: ["nyu"] }, "にょ": { romaji: ["nyo"] },
+    "ひゃ": { romaji: ["hya"] }, "ひゅ": { romaji: ["hyu"] }, "ひょ": { romaji: ["hyo"] },
+    "みゃ": { romaji: ["mya"] }, "みゅ": { romaji: ["myu"] }, "みょ": { romaji: ["myo"] },
+    "りゃ": { romaji: ["rya"] }, "りゅ": { romaji: ["ryu"] }, "りょ": { romaji: ["ryo"] },
+    "ぎゃ": { romaji: ["gya"] }, "ぎゅ": { romaji: ["gyu"] }, "ぎょ": { romaji: ["gyo"] },
+    "じゃ": { romaji: ["ja"] }, "じゅ": { romaji: ["ju"] }, "じょ": { romaji: ["jo"] },
+    "びゃ": { romaji: ["bya"] }, "びゅ": { romaji: ["byu"] }, "びょ": { romaji: ["byo"] },
+    "ぴゃ": { romaji: ["pya"] }, "ぴゅ": { romaji: ["pyu"] }, "ぴょ": { romaji: ["pyo"] }
+  }
+};
---
+++ katakana_core.js
@@ -0,0 +1,45 @@
+const KATAKANA_DATA = {
+ "Katakana Basic": {
+    "ア": { romaji: ["a"] }, "イ": { romaji: ["i"] }, "ウ": { romaji: ["u"] }, "エ": { romaji: ["e"] }, "オ": { romaji: ["o"] },
+    "カ": { romaji: ["ka"] }, "キ": { romaji: ["ki"] }, "ク": { romaji: ["ku"] }, "ケ": { romaji: ["ke"] }, "コ": { romaji: ["ko"] },
+    "サ": { romaji: ["sa"] }, "シ": { romaji: ["shi"] }, "ス": { romaji: ["su"] }, "セ": { romaji: ["se"] }, "ソ": { romaji: ["so"] },
+    "タ": { romaji: ["ta"] }, "チ": { romaji: ["chi"] }, "ツ": { romaji: ["tsu"] }, "テ": { romaji: ["te"] }, "ト": { romaji: ["to"] },
+    "ナ": { romaji: ["na"] }, "ニ": { romaji: ["ni"] }, "ヌ": { romaji: ["nu"] }, "ネ": { romaji: ["ne"] }, "ノ": { romaji: ["no"] },
+    "ハ": { romaji: ["ha"] }, "ヒ": { romaji: ["hi"] }, "フ": { romaji: ["fu"] }, "ヘ": { romaji: ["he"] }, "ホ": { romaji: ["ho"] },
+    "マ": { romaji: ["ma"] }, "ミ": { romaji: ["mi"] }, "ム": { romaji: ["mu"] }, "メ": { romaji: ["me"] }, "モ": { romaji: ["mo"] },
+    "ヤ": { romaji: ["ya"] }, "ユ": { romaji: ["yu"] }, "ヨ": { romaji: ["yo"] },
+    "ラ": { romaji: ["ra"] }, "リ": { romaji: ["ri"] }, "ル": { romaji: ["ru"] }, "レ": { romaji: ["re"] }, "ロ": { romaji: ["ro"] },
+    "ワ": { romaji: ["wa"] }, "ヲ": { romaji: ["wo"] }, "ン": { romaji: ["n"] }
+  },
+
+  "Katakana Dakuon": {
+    "ガ": { romaji: ["ga"] }, "ギ": { romaji: ["gi"] }, "グ": { romaji: ["gu"] }, "ゲ": { romaji: ["ge"] }, "ゴ": { romaji: ["go"] },
+    "ザ": { romaji: ["za"] }, "ジ": { romaji: ["ji"] }, "ズ": { romaji: ["zu"] }, "ゼ": { romaji: ["ze"] }, "ゾ": { romaji: ["zo"] },
+    "ダ": { romaji: ["da"] }, "ヂ": { romaji: ["ji"] }, "ヅ": { romaji: ["zu"] }, "デ": { romaji: ["de"] }, "ド": { romaji: ["do"] },
+    "バ": { romaji: ["ba"] }, "ビ": { romaji: ["bi"] }, "ブ": { romaji: ["bu"] }, "ベ": { romaji: ["be"] }, "ボ": { romaji: ["bo"] },
+    "パ": { romaji: ["pa"] }, "ピ": { romaji: ["pi"] }, "プ": { romaji: ["pu"] }, "ペ": { romaji: ["pe"] }, "ポ": { romaji: ["po"] }
+  },
+
+  "Katakana Yōon": {
+    "キャ": { romaji: ["kya"] }, "キュ": { romaji: ["kyu"] }, "キョ": { romaji: ["kyo"] },
+    "シャ": { romaji: ["sha"] }, "シュ": { romaji: ["shu"] }, "ショ": { romaji: ["sho"] },
+    "チャ": { romaji: ["cha"] }, "チュ": { romaji: ["chu"] }, "チョ": { romaji: ["cho"] },
+    "ニャ": { romaji: ["nya"] }, "ニュ": { romaji: ["nyu"] }, "ニョ": { romaji: ["nyo"] },
+    "ヒャ": { romaji: ["hya"] }, "ヒュ": { romaji: ["hyu"] }, "ヒョ": { romaji: ["hyo"] },
+    "ミャ": { romaji: ["mya"] }, "ミュ": { romaji: ["myu"] }, "ミョ": { romaji: ["myo"] },
+    "リャ": { romaji: ["rya"] }, "リュ": { romaji: ["ryu"] }, "リョ": { romaji: ["ryo"] },
+    "ギャ": { romaji: ["gya"] }, "ギュ": { romaji: ["gyu"] }, "ギョ": { romaji: ["gyo"] },
+    "ジャ": { romaji: ["ja"] }, "ジュ": { romaji: ["ju"] }, "ジョ": { romaji: ["jo"] },
+    "ビャ": { romaji: ["bya"] }, "ビュ": { romaji: ["byu"] }, "ビョ": { romaji: ["byo"] },
+    "ピャ": { romaji: ["pya"] }, "ピュ": { romaji: ["pyu"] }, "ピョ": { romaji: ["pyo"] }
+  },
+   "Katakana Extensions": {
+    "ー": { romaji: ["-", "long vowel", "extend"], meaning: "long vowel mark (chōonpu)" },
+    "ファ": { romaji: ["fa"] }, "フィ": { romaji: ["fi"] }, "フェ": { romaji: ["fe"] }, "フォ": { romaji: ["fo"] },
+    "ヴァ": { romaji: ["va"] }, "ヴィ": { romaji: ["vi"] }, "ヴ": { romaji: ["vu"] }, "ヴェ": { romaji: ["ve"] }, "ヴォ": { romaji: ["vo"] },
+    "ティ": { romaji: ["ti"] }, "ディ": { romaji: ["di"] },
+    "ウィ": { romaji: ["wi"] }, "ウェ": { romaji: ["we"] }, "ウォ": { romaji: ["wo"] },
+    "チェ": { romaji: ["che"] }, "シェ": { romaji: ["she"] }, "ジェ": { romaji: ["je"] },
+    "ツァ": { romaji: ["tsa"] }, "ツィ": { romaji: ["tsi"] }, "ツェ": { romaji: ["tse"] }, "ツォ": { romaji: ["tso"] }
+  }
+};
